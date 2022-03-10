26d5c75b47981129c5190ff8ae3916afc164481f
Corentin <corentin-pro@mail.com>
2022-03-10 17:36:33 +0900

    Initial Commit

---
+++ snippet.py
@@ -0,0 +1,13 @@
+import pyarrow as pa
+from pyarrow import flight
+
+client = pa.flight.connect('grpc+tls://flight.spiceai.io')
+
+headers = [client.authenticate_basic_token('', 'API_KEY')]
+options = flight.FlightCallOptions(headers=headers)
+
+query = 'SELECT * FROM eth.blocks ORDER BY number DESC LIMIT 10;'
+flight_info = client.get_flight_info(flight.FlightDescriptor.for_command(query), options)
+reader = client.do_get(flight_info.endpoints[0].ticket, options)
+
+print(reader.read_pandas())
