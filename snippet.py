18d23eaa4e6aa43c4dfe5ac86eb22ae0e4e6995b
Corentin <corentin-pro@mail.com>
2022-03-23 16:52:52 +0900

    Generic path for windows compatibility
    * User temporary folder for certificate path

--- snippet.py
+++ snippet.py
@@ -1,21 +1,22 @@
 import os
 from pathlib import Path
 import ssl
+import tempfile
 import urllib.request
 
 from pyarrow import flight
 
 
 # Check for gRPC certificates
-if not Path('/usr', 'share', 'grpc', 'roots.pem').exists():
+if not (Path(Path.cwd().absolute().anchor) / 'usr' / 'share' / 'grpc' / 'roots.pem').exists():
     env_name = 'GRPC_DEFAULT_SSL_ROOTS_FILE_PATH'
     if env_name not in os.environ or not Path(os.environ[env_name]).exists():
-        tls_root_certs = './roots.pem'
+        tls_root_certs = Path(tempfile.gettempdir()) / 'roots.pem'
         if not Path(tls_root_certs).exists():
             print('Downloading gRPC certificates')
             ssl._create_default_https_context = ssl._create_unverified_context
-            urllib.request.urlretrieve('https://pki.google.com/roots.pem', tls_root_certs)
-        os.environ[env_name] = tls_root_certs
+            urllib.request.urlretrieve('https://pki.google.com/roots.pem', str(tls_root_certs))
+        os.environ[env_name] = str(tls_root_certs)
 
 client = flight.connect('grpc+tls://flight.spiceai.io')
 headers = [client.authenticate_basic_token('', 'API_KEY')]
