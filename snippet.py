d0a524daa2fe4c765a1f4812f21f6f5df97098a0
corentin <corentin@spice.ai>
2022-04-13 17:48:29 +0200

    Add installation comment

--- snippet.py
+++ snippet.py
@@ -1,5 +1,6 @@
+# install with: pip install git+https://github.com/spicehq/spice-py
 from spicepy import Client
 
 client = Client('API_KEY')
-reader = client.query('SELECT * FROM eth.blocks ORDER BY number DESC LIMIT 10;')
+reader = client.query('SELECT * FROM eth.recent_blocks ORDER BY number DESC;')
 print(reader.read_pandas())
