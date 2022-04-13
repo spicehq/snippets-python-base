514b91e175725fba77d66f8b76bfd0ff469defa7
Phillip LeBlanc <phillip@leblanc.tech>
2022-04-13 11:12:47 +0900

    Update the snippet to have the correct gRPC certs

--- snippet.py
+++ snippet.py
@@ -9,13 +9,12 @@ from pyarrow import flight
 
 # Check for gRPC certificates
 if not (Path(Path.cwd().absolute().anchor) / 'usr' / 'share' / 'grpc' / 'roots.pem').exists():
-    env_name = 'GRPC_DEFAULT_SSL_ROOTS_FILE_PATH'
+    env_name = "GRPC_DEFAULT_SSL_ROOTS_FILE_PATH"
     if env_name not in os.environ or not Path(os.environ[env_name]).exists():
-        tls_root_certs = Path(tempfile.gettempdir()) / 'roots.pem'
+        tls_root_certs = Path(tempfile.gettempdir()) / "isrgrootx1.pem"
         if not Path(tls_root_certs).exists():
-            print('Downloading gRPC certificates')
             ssl._create_default_https_context = ssl._create_unverified_context
-            urllib.request.urlretrieve('https://pki.google.com/roots.pem', str(tls_root_certs))
+            urllib.request.urlretrieve("https://letsencrypt.org/certs/isrgrootx1.pem", str(tls_root_certs))
         os.environ[env_name] = str(tls_root_certs)
 
 client = flight.connect('grpc+tls://flight.spiceai.io')
